e41453abc6eb28211909125a979982aebe950c91
giladadon1997 <gdadon@escribers.net>
2020-11-03 12:23:24 +0200

    use

--- src/hooks/useParticipants/useParticipants.tsx
+++ src/hooks/useParticipants/useParticipants.tsx
@@ -8,6 +8,18 @@ export default function useParticipants() {
   const dominantSpeaker = useDominantSpeaker();
   const [participants, setParticipants] = useState(Array.from(room.participants.values()));
 
+  // When the dominant speaker changes, they are moved to the front of the participants array.
+  // This means that the most recent dominant speakers will always be near the top of the
+  // ParticipantStrip component.
+  useEffect(() => {
+    if (dominantSpeaker) {
+      setParticipants(prevParticipants => [
+        dominantSpeaker,
+        ...prevParticipants.filter(participant => participant !== dominantSpeaker),
+      ]);
+    }
+  }, [dominantSpeaker]);
+
   useEffect(() => {
     const participantConnected = (participant: RemoteParticipant) =>
       setParticipants(prevParticipants => [...prevParticipants, participant]);
