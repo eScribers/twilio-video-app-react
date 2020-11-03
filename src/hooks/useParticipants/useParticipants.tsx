0cc4ced52243adf67768e1af3213a681f78e88bf
giladadon1997 <gdadon@escribers.net>
2020-11-03 08:46:30 +0200

    fix

--- src/hooks/useParticipants/useParticipants.tsx
+++ src/hooks/useParticipants/useParticipants.tsx
@@ -6,31 +6,13 @@ import useVideoContext from '../useVideoContext/useVideoContext';
 export default function useParticipants() {
   const { room } = useVideoContext();
   const dominantSpeaker = useDominantSpeaker();
-    const [participants, setParticipants] = useState(
-        Array.from(room.participants.values()));
-
-  // When the dominant speaker changes, they are moved to the front of the participants array.
-  // This means that the most recent dominant speakers will always be near the top of the
-  // ParticipantStrip component.
-  useEffect(() => {
-    if (dominantSpeaker) {
-      setParticipants(prevParticipants => [
-        dominantSpeaker,
-          ...prevParticipants.filter(
-              participant => participant !== dominantSpeaker
-          ),
-      ]);
-    }
-  },
-      [dominantSpeaker]);
+  const [participants, setParticipants] = useState(Array.from(room.participants.values()));
 
   useEffect(() => {
     const participantConnected = (participant: RemoteParticipant) =>
       setParticipants(prevParticipants => [...prevParticipants, participant]);
     const participantDisconnected = (participant: RemoteParticipant) =>
-        setParticipants(prevParticipants =>
-            prevParticipants.filter(p => p !== participant)
-        );
+      setParticipants(prevParticipants => prevParticipants.filter(p => p !== participant));
     room.on('participantConnected', participantConnected);
     room.on('participantDisconnected', participantDisconnected);
     return () => {
