c5d0bc6fc3a766e59cf329e43cac70492e9bc00c
giladadon1997 <gdadon@escribers.net>
2021-01-24 11:31:38 +0200

    add

--- src/components/MenuBar/SettingsButton/SettingsButton.tsx
+++ src/components/MenuBar/SettingsButton/SettingsButton.tsx
@@ -1,186 +1,239 @@
-import React, { useState, useEffect } from 'react';
-import { ValueType, OptionType } from 'react-select';
-import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  IconButton,
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem,
-} from '@material-ui/core';
+import React, { useState } from 'react';
+import { Button, Dialog, DialogActions, Tab, Tabs, Theme } from '@material-ui/core';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import ConnectionOptions from '../ConnectionOptions/ConnectionOptions';
+import { DeviceSelector } from '../DeviceSelector/DeviceSelector';
 
-import { makeStyles, Theme } from '@material-ui/core/styles';
-
-import SettingsIcon from '@material-ui/icons/Settings';
-
-import useMediaDevices from '../../../hooks/useMediaDevices/useMediaDevices';
-import { useAppState } from '../../../state';
-
-const useStyles = makeStyles((theme: Theme) => ({
-  root: {
-    '& .MuiFormControl-root': {
-      marginBottom: '20px',
-      width: '100%',
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      width: '525px',
+      minHeight: '400px',
+      [theme.breakpoints.down('xs')]: {
+        width: 'calc(100vw - 32px)',
+      },
+      '& .inputSelect': {
+        width: 'calc(100% - 35px)',
+      },
     },
-  },
-}));
-export default function SettingsButton(selectedAudioDevice: any, selectedVideoDevice: any) {
+    button: {
+      float: 'right',
+    },
+    paper: {
+      [theme.breakpoints.down('xs')]: {
+        margin: '16px',
+      },
+    },
+  })
+);
+
+export default function SettingsDialog({ open, onClose }: { open: boolean; onClose: () => void }) {
   const classes = useStyles();
+  const [selectedTab, setSelectedTab] = useState(0);
 
-  const [open, setOpen] = useState(false);
-  const {
-    setSelectedAudioInput,
-    setSelectedVideoInput,
-    selectedSpeakerOutput,
-    setSelectedSpeakerOutput,
-  } = useAppState();
-
-  const [selectedMic, setSelectedMic] = useState(selectedAudioDevice);
-  const [selectedCamera, setSelectedCamera] = useState(selectedVideoDevice);
-  const [selectedSpeaker, setSelectedSpeaker] = useState(selectedSpeakerOutput ? selectedSpeakerOutput : '');
-
-  useEffect(() => {
-    setSelectedMic(selectedAudioDevice);
-  }, [selectedAudioDevice]);
-
-  useEffect(() => {
-    setSelectedCamera(selectedVideoDevice);
-  }, [selectedVideoDevice]);
-
-  useEffect(() => {
-    setSelectedSpeaker(selectedSpeaker);
-  }, [selectedSpeaker]);
-
-  const { devices: audioinputs } = useMediaDevices('audioinput');
-
-  const { devices: videoinputs } = useMediaDevices('videoinput');
-
-  const { devices: audiooutput } = useMediaDevices('audiooutput');
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const handleSubmit = () => {
-    if (selectedAudioDevice !== selectedMic) {
-      setSelectedAudioInput(selectedMic);
-    }
-    if (selectedVideoDevice !== selectedCamera) {
-      setSelectedVideoInput(selectedCamera);
-    }
-    if (selectedSpeaker && selectedSpeaker.deviceId) {
-      if (selectedSpeakerOutput && selectedSpeakerOutput.deviceId !== selectedSpeaker.deviceId) {
-        setSelectedSpeakerOutput(selectedSpeaker);
-      }
-    }
-    setOpen(false);
-  };
-
-  const handleAudioInputChange = (target: ValueType<OptionType>) => {
-    const selectedDeviceInfo = audioinputs.find((x: { deviceId: any }) => (x.deviceId === x.deviceId) === target.value);
-    setSelectedMic(selectedDeviceInfo);
-  };
-
-  const handleVideoInputChange = (target: ValueType<OptionType>) => {
-    const selectedDeviceInfo = videoinputs.find((x: { deviceId: any }) => x.deviceId === target.value);
-    setSelectedCamera(selectedDeviceInfo);
-  };
-
-  const handleSpeakerInputChange = (target: ValueType<OptionType>) => {
-    const selectedDeviceInfo = audiooutput.find((x: { deviceId: any }) => x.deviceId === target.value);
-    setSelectedSpeaker(selectedDeviceInfo);
+  const handleChange = (_: React.ChangeEvent<{}>, newValue: number) => {
+    setSelectedTab(newValue);
   };
 
   return (
-    <>
-      <IconButton onClick={handleClickOpen}>
-        <SettingsIcon />
-      </IconButton>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="form-dialog-title"
-        className={classes.root}
-        fullWidth={true}
-        maxWidth={'sm'}
-      >
-        <DialogTitle id="form-dialog-title">Settings</DialogTitle>
-        <DialogContent>
-          <FormControl>
-            <InputLabel htmlFor="microphone">Microphone</InputLabel>
-            <Select
-              id="microphone"
-              label="Microphone"
-              value={selectedMic.deviceId}
-              margin="dense"
-              placeholder="Microphone"
-              onChange={handleAudioInputChange}
-            >
-              {audioinputs
-                .filter((x: { deviceId: string }) => x.deviceId !== 'default')
-                .map((input: any) => (
-                  <MenuItem key={input.deviceId} value={input.deviceId}>
-                    {input.label}
-                  </MenuItem>
-                ))}
-            </Select>
-          </FormControl>
-          <FormControl>
-            <InputLabel htmlFor="camera">Camera</InputLabel>
-            <Select
-              id="camera"
-              label="Camera"
-              value={selectedCamera.deviceId}
-              margin="dense"
-              placeholder="Camera"
-              onChange={handleVideoInputChange}
-            >
-              {videoinputs
-                .filter((x: { deviceId: string }) => x.deviceId !== 'default')
-                .map((input: any) => (
-                  <MenuItem key={input.deviceId} value={input.deviceId}>
-                    {input.label}
-                  </MenuItem>
-                ))}
-            </Select>
-          </FormControl>
-          <FormControl>
-            <InputLabel htmlFor="speaker">Speaker</InputLabel>
-            <Select
-              id="speaker"
-              label="Speaker"
-              value={selectedSpeaker.deviceId}
-              margin="dense"
-              placeholder="Speaker"
-              onChange={handleSpeakerInputChange}
-            >
-              {audiooutput
-                .filter((x: { deviceId: string }) => x.deviceId !== 'default')
-                .map((input: any) => (
-                  <MenuItem key={input.deviceId} value={input.deviceId}>
-                    {input.label}
-                  </MenuItem>
-                ))}
-            </Select>
-          </FormControl>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleSubmit} color="primary">
-            Submit
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </>
+    <Dialog open={open} onClose={onClose} classes={{ paper: classes.paper }}>
+      <Tabs value={selectedTab} onChange={handleChange}>
+        <Tab label="Devices" />
+        <Tab label="Settings" />
+      </Tabs>
+      <DeviceSelector className={classes.container} hidden={selectedTab !== 0} />
+      <ConnectionOptions className={classes.container} hidden={selectedTab !== 1} />
+      <DialogActions>
+        <Button className={classes.button} onClick={onClose}>
+          Done
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 }
+// import React, { useState, useEffect } from 'react';
+// import { ValueType, OptionType } from 'react-select';
+// import {
+//   Button,
+//   Dialog,
+//   DialogActions,
+//   DialogContent,
+//   DialogTitle,
+//   IconButton,
+//   FormControl,
+//   InputLabel,
+//   Select,
+//   MenuItem,
+// } from '@material-ui/core';
+
+// import { makeStyles, Theme } from '@material-ui/core/styles';
+
+// import SettingsIcon from '@material-ui/icons/Settings';
+
+// import useMediaDevices from '../../../hooks/useMediaDevices/useMediaDevices';
+// import { useAppState } from '../../../state';
+
+// const useStyles = makeStyles((theme: Theme) => ({
+//   root: {
+//     '& .MuiFormControl-root': {
+//       marginBottom: '20px',
+//       width: '100%',
+//     },
+//   },
+// }));
+// export default function SettingsButton(selectedAudioDevice: any, selectedVideoDevice: any) {
+//   const classes = useStyles();
+
+//   const [open, setOpen] = useState(false);
+//   const {
+//     setSelectedAudioInput,
+//     setSelectedVideoInput,
+//     selectedSpeakerOutput,
+//     setSelectedSpeakerOutput,
+//   } = useAppState();
+
+//   const [selectedMic, setSelectedMic] = useState(selectedAudioDevice);
+//   const [selectedCamera, setSelectedCamera] = useState(selectedVideoDevice);
+//   const [selectedSpeaker, setSelectedSpeaker] = useState(selectedSpeakerOutput ? selectedSpeakerOutput : '');
+
+//   useEffect(() => {
+//     setSelectedMic(selectedAudioDevice);
+//   }, [selectedAudioDevice]);
+
+//   useEffect(() => {
+//     setSelectedCamera(selectedVideoDevice);
+//   }, [selectedVideoDevice]);
+
+//   useEffect(() => {
+//     setSelectedSpeaker(selectedSpeaker);
+//   }, [selectedSpeaker]);
+
+//   const { devices: audioinputs } = useMediaDevices('audioinput');
+
+//   const { devices: videoinputs } = useMediaDevices('videoinput');
+
+//   const { devices: audiooutput } = useMediaDevices('audiooutput');
+
+//   const handleClickOpen = () => {
+//     setOpen(true);
+//   };
+
+//   const handleClose = () => {
+//     setOpen(false);
+//   };
+
+//   const handleSubmit = () => {
+//     if (selectedAudioDevice !== selectedMic) {
+//       setSelectedAudioInput(selectedMic);
+//     }
+//     if (selectedVideoDevice !== selectedCamera) {
+//       setSelectedVideoInput(selectedCamera);
+//     }
+//     if (selectedSpeaker && selectedSpeaker.deviceId) {
+//       if (selectedSpeakerOutput && selectedSpeakerOutput.deviceId !== selectedSpeaker.deviceId) {
+//         setSelectedSpeakerOutput(selectedSpeaker);
+//       }
+//     }
+//     setOpen(false);
+//   };
+
+//   const handleAudioInputChange = (target: ValueType<OptionType>) => {
+//     const selectedDeviceInfo = audioinputs.find((x: { deviceId: any }) => (x.deviceId === x.deviceId) === target.value);
+//     setSelectedMic(selectedDeviceInfo);
+//   };
+
+//   const handleVideoInputChange = (target: ValueType<OptionType>) => {
+//     const selectedDeviceInfo = videoinputs.find((x: { deviceId: any }) => x.deviceId === target.value);
+//     setSelectedCamera(selectedDeviceInfo);
+//   };
+
+//   const handleSpeakerInputChange = (target: ValueType<OptionType>) => {
+//     const selectedDeviceInfo = audiooutput.find((x: { deviceId: any }) => x.deviceId === target.value);
+//     setSelectedSpeaker(selectedDeviceInfo);
+//   };
+
+//   return (
+//     <>
+//       <IconButton onClick={handleClickOpen}>
+//         <SettingsIcon />
+//       </IconButton>
+//       <Dialog
+//         open={open}
+//         onClose={handleClose}
+//         aria-labelledby="form-dialog-title"
+//         className={classes.root}
+//         fullWidth={true}
+//         maxWidth={'sm'}
+//       >
+//         <DialogTitle id="form-dialog-title">Settings</DialogTitle>
+//         <DialogContent>
+//           <FormControl>
+//             <InputLabel htmlFor="microphone">Microphone</InputLabel>
+//             <Select
+//               id="microphone"
+//               label="Microphone"
+//               value={selectedMic.deviceId}
+//               margin="dense"
+//               placeholder="Microphone"
+//               onChange={handleAudioInputChange}
+//             >
+//               {audioinputs
+//                 .filter((x: { deviceId: string }) => x.deviceId !== 'default')
+//                 .map((input: any) => (
+//                   <MenuItem key={input.deviceId} value={input.deviceId}>
+//                     {input.label}
+//                   </MenuItem>
+//                 ))}
+//             </Select>
+//           </FormControl>
+//           <FormControl>
+//             <InputLabel htmlFor="camera">Camera</InputLabel>
+//             <Select
+//               id="camera"
+//               label="Camera"
+//               value={selectedCamera.deviceId}
+//               margin="dense"
+//               placeholder="Camera"
+//               onChange={handleVideoInputChange}
+//             >
+//               {videoinputs
+//                 .filter((x: { deviceId: string }) => x.deviceId !== 'default')
+//                 .map((input: any) => (
+//                   <MenuItem key={input.deviceId} value={input.deviceId}>
+//                     {input.label}
+//                   </MenuItem>
+//                 ))}
+//             </Select>
+//           </FormControl>
+//           <FormControl>
+//             <InputLabel htmlFor="speaker">Speaker</InputLabel>
+//             <Select
+//               id="speaker"
+//               label="Speaker"
+//               value={selectedSpeaker.deviceId}
+//               margin="dense"
+//               placeholder="Speaker"
+//               onChange={handleSpeakerInputChange}
+//             >
+//               {audiooutput
+//                 .filter((x: { deviceId: string }) => x.deviceId !== 'default')
+//                 .map((input: any) => (
+//                   <MenuItem key={input.deviceId} value={input.deviceId}>
+//                     {input.label}
+//                   </MenuItem>
+//                 ))}
+//             </Select>
+//           </FormControl>
+//         </DialogContent>
+//         <DialogActions>
+//           <Button onClick={handleClose} color="primary">
+//             Cancel
+//           </Button>
+//           <Button onClick={handleSubmit} color="primary">
+//             Submit
+//           </Button>
+//         </DialogActions>
+//       </Dialog>
+//     </>
+//   );
+// }
