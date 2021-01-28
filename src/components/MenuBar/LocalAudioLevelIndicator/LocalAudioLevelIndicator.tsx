6e4ab2ee03eded743c4711925e60d75e3734b284
Gal Zakay <gal@nexil.co.il>
2021-01-28 19:01:26 +0200

    CA-583: Header volume indicator is now clickable (mute/unmute)

--- src/components/MenuBar/LocalAudioLevelIndicator/LocalAudioLevelIndicator.tsx
+++ src/components/MenuBar/LocalAudioLevelIndicator/LocalAudioLevelIndicator.tsx
@@ -6,5 +6,5 @@ import { TRACK_TYPE } from '../../../utils/displayStrings';
 export default function LocalAudioLevelIndicator() {
   const { localTracks } = useVideoContext();
   const audioTrack = localTracks.find(track => track.kind === TRACK_TYPE.AUDIO) as LocalAudioTrack;
-  return <AudioLevelIndicator size={30} audioTrack={audioTrack} />;
+  return <AudioLevelIndicator size={24} audioTrack={audioTrack} />;
 }
