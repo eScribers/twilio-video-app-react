ce02fd6e0af42f36a400167d3a4ccbf17cfb7a89
Galzk <gal@nexil.co.il>
2021-01-31 15:43:32 +0200

    Fixed test for AudioLevelIndicator

--- src/components/AudioLevelIndicator/AudioLevelIndicator.test.tsx
+++ src/components/AudioLevelIndicator/AudioLevelIndicator.test.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
 import AudioLevelIndicator from './AudioLevelIndicator';
 import MicOff from '@material-ui/icons/MicOff';
 import useIsTrackEnabled from '../../hooks/useIsTrackEnabled/useIsTrackEnabled';
+import useVideoContext from '../../hooks/useVideoContext/useVideoContext';
 
 jest.mock('../../hooks/useIsTrackEnabled/useIsTrackEnabled');
+jest.mock('../../hooks/useVideoContext/useVideoContext');
 
+// @ts-ignore
+const mockedUseVideoContext = useVideoContext as jest.Mock<IVideoContext>;
 const mockUseIsTrackEnabled = useIsTrackEnabled as jest.Mock<boolean>;
 
 describe('the AudioLevelIndicator component', () => {
   it('should render a MicOff icon when the audioTrack is not enabled', () => {
     mockUseIsTrackEnabled.mockImplementationOnce(() => false);
+    mockedUseVideoContext.mockImplementation(() => ({ isConnecting: false, room: {}, localTracks: [] } as any));
     const wrapper = shallow(<AudioLevelIndicator />);
     expect(wrapper.exists(MicOff)).toBe(true);
   });
 
   it('should not render a MicOff icon when the audioTrack is enabled', () => {
     mockUseIsTrackEnabled.mockImplementationOnce(() => true);
+    mockedUseVideoContext.mockImplementation(() => ({ isConnecting: false, room: {}, localTracks: [] } as any));
     const wrapper = shallow(<AudioLevelIndicator />);
     expect(wrapper.exists(MicOff)).toBe(false);
-    expect(wrapper.exists('svg')).toBe(true);
+    expect(wrapper.find('svg').exists()).toBe(true);
   });
 
   it('should change the size of the indicator when the size prop is used', () => {
