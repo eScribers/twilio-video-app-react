7651636bcf206e8fd5fbdb65fe9e644399d67a35
giladadon1997 <gdadon@escribers.net>
2021-01-31 09:55:17 +0200

    update

--- src/components/ParticipantStrip/ParticipantStrip.tsx
+++ src/components/ParticipantStrip/ParticipantStrip.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { styled } from '@material-ui/core/styles';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import classNames from 'classnames';
 import Grid from '@material-ui/core/Grid';
+import GridList from '@material-ui/core/GridList';
 import { VIEW_MODE } from '../../state/settings/settingsReducer';
 import useParticipants from '../../hooks/useParticipants/useParticipants';
 import useVideoContext from '../../hooks/useVideoContext/useVideoContext';
@@ -12,11 +14,20 @@ import Participant from '../Participant/Participant';
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
+    display: 'flex',
+    height: '100%',
+    [theme.breakpoints.down('xs')]: {
+      flexWrap: 'wrap',
+    },
+    flexWrap: 'nowrap',
   },
   paper: {
     padding: theme.spacing(1),
     textAlign: 'center',
     color: theme.palette.text.secondary,
+  },
+  scrollable: {
+    overflow: 'auto',
   },
 }));
 
@@ -37,6 +48,12 @@ const ScrollContainer = styled('div')(({ theme }) => ({
   },
 }));
 
+// const ScrollGrid = styled('div')(({ theme }) => ({
+//   [theme.breakpoints.down('xs')]: {
+//     display: 'flex',
+//   },
+// }));
+
 interface ParticipantStripProps {
   viewMode: string;
 }
@@ -52,7 +69,7 @@ export default function ParticipantStrip({ viewMode }: ParticipantStripProps) {
   return viewMode === VIEW_MODE.grid ? (
     <div className={classes.root}>
       <Grid container spacing={3}>
-        <Grid item xs={4} sm={4} md={4} lg={4}>
+        <Grid item xs={4} sm={4} md={4} lg={4} className={classes.scrollable}>
           <Paper className={classes.paper}>
             <Participant
               gridView={true}
@@ -63,17 +80,15 @@ export default function ParticipantStrip({ viewMode }: ParticipantStripProps) {
           </Paper>
         </Grid>
         {participants.map((participant: any) => (
-          <Grid key={participant.sid} item xs={4} sm={4} md={4} lg={4}>
+          <Grid key={participant.sid} item xs={4} sm={4} md={4} lg={4} className={classes.scrollable}>
             <Paper className={classes.paper}>
-              <ScrollContainer>
-                <Participant
-                  gridView={true}
-                  key={participant.sid}
-                  participant={participant}
-                  isSelected={selectedParticipant === participant}
-                  onClick={() => setSelectedParticipant(participant)}
-                />
-              </ScrollContainer>
+              <Participant
+                gridView={true}
+                key={participant.sid}
+                participant={participant}
+                isSelected={selectedParticipant === participant}
+                onClick={() => setSelectedParticipant(participant)}
+              />
             </Paper>
           </Grid>
         ))}
