4845de262e2c173aa156d12d8d6f5494fdd449b4
Gal Zakay <gal@nexil.co.il>
2021-03-18 11:55:17 +0200

    CA-836 Fixed video height when in portrait mode

--- src/components/LocalVideoPreview/LocalVideoPreview.tsx
+++ src/components/LocalVideoPreview/LocalVideoPreview.tsx
@@ -20,6 +20,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     width: '100%',
     height: '100%',
     maxHeight: '100vh',
+    display: 'flex',
   },
   identityContainer: {
     position: 'absolute',
