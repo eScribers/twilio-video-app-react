3152a45a03c1d38e3f027502d01b70a631051ff9
Gal Zakay <gal@nexil.co.il>
2021-03-24 16:56:32 +0200

    CA-541 Added buildId to the about popup

--- src/components/MenuBar/AboutDialog/AboutDialog.test.tsx
+++ src/components/MenuBar/AboutDialog/AboutDialog.test.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import AboutDialog from './AboutDialog';
 import { render } from '@testing-library/react';
+import { useAppState } from '../../../state';
 
 jest.mock('twilio-video', () => ({ version: '1.2', isSupported: true }));
 jest.mock('../../../../package.json', () => ({ version: '1.3' }));
+jest.mock('../../../state');
 
+const mockUseAppState = useAppState as jest.Mock<any>;
+mockUseAppState.mockImplementation(() => ({ activeSinkId: '' }));
 describe('the AboutDialog component', () => {
   it('should display Video.isSupported', () => {
     const { getByText } = render(<AboutDialog open={true} onClose={() => {}} />);
@@ -19,42 +23,5 @@ describe('the AboutDialog component', () => {
   it('should display the package.json version', () => {
     const { getByText } = render(<AboutDialog open={true} onClose={() => {}} />);
     expect(getByText('App Version: 1.3')).toBeTruthy();
-  });
-
-  describe('when running locally', () => {
-    beforeEach(() => {
-      // @ts-ignore
-      process.env = {};
-    });
-
-    it('should display N/A as the git tag', () => {
-      const { getByText } = render(<AboutDialog open={true} onClose={() => {}} />);
-      expect(getByText('Deployed Tag: N/A')).toBeTruthy();
-    });
-
-    it('should disaply N/A as the commit hash', () => {
-      const { getByText } = render(<AboutDialog open={true} onClose={() => {}} />);
-      expect(getByText('Deployed Commit Hash: N/A')).toBeTruthy();
-    });
-  });
-
-  describe('when deployed via CircleCI', () => {
-    beforeEach(() => {
-      // @ts-ignore
-      process.env = {
-        REACT_APP_GIT_TAG: 'v0.1',
-        REACT_APP_GIT_COMMIT: '01b2c3',
-      };
-    });
-
-    it('should display the git tag', () => {
-      const { getByText } = render(<AboutDialog open={true} onClose={() => {}} />);
-      expect(getByText('Deployed Tag: v0.1')).toBeTruthy();
-    });
-
-    it('should display the commit hash', () => {
-      const { getByText } = render(<AboutDialog open={true} onClose={() => {}} />);
-      expect(getByText('Deployed Commit Hash: 01b2c3')).toBeTruthy();
-    });
   });
 });
