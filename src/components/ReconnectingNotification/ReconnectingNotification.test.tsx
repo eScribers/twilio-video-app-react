4b2148dd214355e4bbfa9c516f5a10224d4d9e95
Gal Zakay <gal@nexil.co.il>
2021-06-24 17:48:33 +0300

    CA-983 tests fix for ReconnectingNotification

--- src/components/ReconnectingNotification/ReconnectingNotification.test.tsx
+++ src/components/ReconnectingNotification/ReconnectingNotification.test.tsx
@@ -1,20 +1,30 @@
+import rootStore, { RootStore } from '../../stores/makeStore';
 import React from 'react';
 import ReconnectingNotification from './ReconnectingNotification';
 import { shallow } from 'enzyme';
-import useRoomState from '../../hooks/useRoomState/useRoomState';
 
-jest.mock('../../hooks/useRoomState/useRoomState');
-const mockUseRoomState = useRoomState as jest.Mock<string>;
+jest.mock('../../stores', () => {
+  return {
+    __esModule: true, // this property makes it work
+    default: rootStore,
+  };
+});
 
 describe('the ReconnectingNotification component', () => {
+  beforeEach(() => {
+    let newStore = new RootStore();
+    rootStore.participantStore = newStore.participantStore;
+    rootStore.roomStore = newStore.roomStore;
+  });
+
   it('should not open Snackbar when room state is not "reconnecting"', () => {
-    mockUseRoomState.mockImplementation(() => 'connected');
+    rootStore.roomStore.room.state = 'connected';
     const wrapper = shallow(<ReconnectingNotification />);
     expect(wrapper.find({ open: false }).exists()).toBe(true);
   });
 
   it('should open Snackbar when room state is "reconnecting"', () => {
-    mockUseRoomState.mockImplementation(() => 'reconnecting');
+    rootStore.roomStore.room.state = 'reconnecting';
     const wrapper = shallow(<ReconnectingNotification />);
     expect(wrapper.find({ open: true }).exists()).toBe(true);
   });
