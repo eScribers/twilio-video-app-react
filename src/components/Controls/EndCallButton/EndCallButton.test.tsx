bf1a58bea191715fb20673b61faa059ed1937c3e
Gal Zakay <gal@nexil.co.il>
2021-06-28 09:58:52 +0300

    CA-983 tests fix for EndCallButton

--- src/components/Controls/EndCallButton/EndCallButton.test.tsx
+++ src/components/Controls/EndCallButton/EndCallButton.test.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import EndCallButton from './EndCallButton';
-
-const mockRoom: any = { disconnect: jest.fn() };
-jest.mock('../../../hooks/useVideoContext/useVideoContext', () => () => ({ room: mockRoom }));
+import rootStore from '../../../stores';
+import { mockRoom } from '../../../__mocks__/twilio-video';
 
 describe('End Call button', () => {
   it('should disconnect from the room when clicked', () => {
+    // @ts-expect-error
+    rootStore.roomStore.room = mockRoom;
+
+    if (rootStore.roomStore.room) jest.spyOn(rootStore.roomStore.room, 'disconnect');
     const wrapper = shallow(<EndCallButton />);
     wrapper.simulate('click');
-    expect(mockRoom.disconnect).toHaveBeenCalled();
+    expect(rootStore.roomStore.room.disconnect).toHaveBeenCalled();
   });
 });
